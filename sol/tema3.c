cb8c713288c0d4a295009383d81cd59fb02793ba
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2022-01-17 00:17:30 +0200

    readme

--- sol/tema3.c
+++ sol/tema3.c
@@ -46,7 +46,6 @@ int main(int argc, char * argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
 
-	// citit, mesaje, topo
 	if (rank == ROOT) {
 		readFromCluster(cluster, rank);
 		MPI_Send(&rank, 1, MPI_INT, ROOT+1, 0, MPI_COMM_WORLD);
